e543ac87826a5a322dab7fefa5b7393645f41537
Kevin Toala <kevin.toala@outlook.com>
2018-05-19 01:49:41 -0500

    Configuraciones Docker - Geolocation producer finalizada

--- geolocation-producer/src/main/java/com/lesstraffic/geolocationproducer/GeolocationProducerApplication.java
+++ geolocation-producer/src/main/java/com/lesstraffic/geolocationproducer/GeolocationProducerApplication.java
@@ -3,12 +3,20 @@ package com.lesstraffic.geolocationproducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class GeolocationProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GeolocationProducerApplication.class, args);
+    }
+    
+    @GetMapping("/test")
+	public String test(){
+    	return "ok!!";
     }
 }
