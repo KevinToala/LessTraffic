5e3103bc1ef61e9a51d444d9f0f12b33eb5d0ce1
Kevin Toala <kevin.toala@outlook.com>
2018-05-19 01:24:50 -0500

    Configuraciones Docker - Gateway server y Geolocation-producer

--- gateway-server/src/main/java/com/lesstraffic/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/lesstraffic/gatewayserver/GatewayServerApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+    
+    @GetMapping("/test")
+	public String test(){
+    	return "Test!!!";
     }
 }
